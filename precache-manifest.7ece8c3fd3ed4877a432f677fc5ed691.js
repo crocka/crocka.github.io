529a5a223d2fb3ac642c52706ac61fe728e1b52d
crocka <chino123anc@hotmail.com>
2022-04-04 22:00:33 -0400

    Updates

---
+++ precache-manifest.7ece8c3fd3ed4877a432f677fc5ed691.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0f34cc4681ef02e947e61a218b8580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb016a6bcde086baaf90",
+    "url": "/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "d62151f48bd57cd75e76",
+    "url": "/static/css/main.259b522b.chunk.css"
+  },
+  {
+    "revision": "fb016a6bcde086baaf90",
+    "url": "/static/js/2.e2eb8bf3.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/static/js/2.e2eb8bf3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d62151f48bd57cd75e76",
+    "url": "/static/js/main.72471541.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "27474894e97eef6ddf9b2c9cbbda391f",
+    "url": "/static/media/jungle.27474894.png"
+  },
+  {
+    "revision": "c8b3111f31b9f984d65fb007be33d5f5",
+    "url": "/static/media/male.c8b3111f.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/static/media/myImage.bd80794f.png"
+  },
+  {
+    "revision": "e0520b7cfd951a899e4dfadc2c3f5326",
+    "url": "/static/media/onlyfriendz.e0520b7c.gif"
+  },
+  {
+    "revision": "6c0bae2d65c0b1bd797bf2a387c9c0cf",
+    "url": "/static/media/quiztacular.6c0bae2d.png"
+  },
+  {
+    "revision": "c5b3aa38a7c545e1697aa8d08359eae4",
+    "url": "/static/media/scheduler.c5b3aa38.png"
+  },
+  {
+    "revision": "50a1477e7baee4536e10818fd6a440d4",
+    "url": "/static/media/tinyapp.50a1477e.png"
+  },
+  {
+    "revision": "e81014977f0193f193ab23296cd5b3f1",
+    "url": "/static/media/tweeter.e8101497.png"
+  }
+]);
